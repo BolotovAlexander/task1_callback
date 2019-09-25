3340cac106b9cca05000a42578bdd2d05588caf6
Alexander <bolotovalexander57@gmail.com>
2019-09-25 16:10:16 +0300

    made logic skeleton

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-﻿let startKnob = document.querySelector(".selectButton");
+﻿window.onload= () =>{
+
+let startKnob = document.querySelector(".selectButton");
+let modalWindowAnswer = true;
+let answer = '';
+
+startKnob.addEventListener("click", clickOnKnob)
+
+function clickOnKnob(){
+    demandShowModalWindow();
+   /*  showLoader();
+    fetchWeather(); */
+};
+
+function demandShowModalWindow(){
+    if (modalWindowAnswer) { 
+        showModalWindow();
+        debugger;
+    if (answer = 'cancel') return};
+    console.log('asfsaf')
+    modalWindowAnswer = false;
+    };
+
+function showModalWindow(){
+alert ('модалочка')
+return answer
+};
+
+
+/* fucntion showLoader(){
+    fetchWeather();
+};
+
+function fetchWeather(){
+
+}; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
