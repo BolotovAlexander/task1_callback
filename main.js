df3ee829bea244c4524b2dde1e1cfc4300e13118
Alexander <bolotovalexander57@gmail.com>
2019-09-26 23:58:47 +0300

    Working on functionality

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 ﻿window.onload= () =>{
 
 let startKnob = document.querySelector(".selectButton");
+let wrapperModalWindow = document.querySelector(".wrapperModalWindow");
+let modalQuestion = document.createElement('div');
+let answerYes = document.createElement('div');
+let answerNo = document.createElement('div');
+let loading = document.createElement('div');
 let modalWindowAnswer = true;
-let answer = '';
+let answer = 'cancel';
+
+
 
 startKnob.addEventListener("click", clickOnKnob)
 
 function clickOnKnob(){
     demandShowModalWindow();
+   /*  if (answer == 'cancel') return;
+    alert("end") */ 
    /*  showLoader();
     fetchWeather(); */
 };
@@ -15,27 +24,56 @@ function clickOnKnob(){
 function demandShowModalWindow(){
     if (modalWindowAnswer) { 
         showModalWindow();
-        debugger;
-    if (answer = 'cancel') return};
-    console.log('asfsaf')
-    modalWindowAnswer = false;
+        
+    if (answer == 'cancel') return};
+    
     };
 
 function showModalWindow(){
-alert ('модалочка')
-return answer
+    modalQuestion.className = "modalQuestion";
+    modalQuestion.innerHTML = "Вы уверены, что хотите получить данные о погоде?";
+    wrapperModalWindow.prepend(modalQuestion);
+
+    answerYes.className = "answerYes";
+    answerYes.innerHTML = "Ага";
+    wrapperModalWindow.append(answerYes);
+    
+    answerNo.className = "answerNo";
+    answerNo.innerHTML = "Неа";
+    wrapperModalWindow.append(answerNo);
+    yesOrNoListener();
+}; 
+
+function yesOrNoListener(){
+    answerYes.addEventListener("click", fetchWeather);
+    answerNo.addEventListener("click", closeModal);
+    
 };
 
 
-/* fucntion showLoader(){
-    fetchWeather();
+function closeModal(){
+    answerYes.removeEventListener("click", fetchWeather);
+    answerNo.removeEventListener("click", closeModal);
+    answerNo.remove();
+    answerYes.remove();
+    modalQuestion.remove();
+
 };
+
+
 
 function fetchWeather(){
+    showLoader();
+    modalWindowAnswer = !modalWindowAnswer;
+    alert("dgdsg")
+}; 
 
-}; */
-
-
+function showLoader(){
+    closeModal();
+    loading.className = "loading";
+    loading.innerHTML = "Loading...";
+    document.body.append(loading);
+}
 
 
 
